e63391ffecff76ea0b4acd1498014f1c6a7efffd
Thiago_Moreira <164171664+ThiagoDevMoreira@users.noreply.github.com>
2025-03-26 06:18:23 -0300

    Update somaDeNumerosCosnecutivos.py

--- somaDeNumerosCosnecutivos.py
+++ somaDeNumerosCosnecutivos.py
@@ -1,9 +1,15 @@
-indice = 13
-soma = 0
-k = 0
+# Script em pyton correspondente ao exercício 1
+def calcular_soma(numero):
+    soma = 0
+    k = 0
+    
+    while k < numero:
+        k += 1
+        soma += k
+    
+    return soma
+    
+numero = int(input("Digite um número inteiro: "))
+resultado = calcular_soma(numero)
 
-while k < indice:
-    k += 1
-    soma += k
-
-print("O valor de soma é:", soma)
+print(f"O valor da soma dos números de 1 a {numero} é: {resultado}")
