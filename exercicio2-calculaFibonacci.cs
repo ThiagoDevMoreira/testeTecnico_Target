31d8ec34110dbe2e4e18aa81da3564172b81e185
Thiago_Moreira <164171664+ThiagoDevMoreira@users.noreply.github.com>
2025-03-26 06:49:31 -0300

    Update exercicio2-calculaFibonacci.cs

--- exercicio2-calculaFibonacci.cs
+++ exercicio2-calculaFibonacci.cs
@@ -5,32 +5,23 @@ class Program
     static void Main() {
         Console.Write("Digite um número inteiro: ");
         int numero = Console.ReadLine()
-
         bool ehFibonacci = Verificar(numero);
-
+	    
         if (ehFibonacci) {
             Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
-        }
-        else {
+        } else {
             Console.WriteLine($"O número {numero} NÃO pertence à sequência de Fibonacci.");
         }
     }
 
-    static bool Verificar(int numero)
-    {
+    static bool Verificar(int numero) {
         int a = 0, b = 1;
-
         while (b <= numero) {
-		
-            if (b == numero) {
-                return true;
-            }
-			
+            if (b == numero) {return true;}		
             int aux = a;
             a = b;
             b = temp + b;
         }
-		
         return false; 
     }
 }
