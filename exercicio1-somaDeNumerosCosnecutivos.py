9b4b72c740b8c0840d9d871222f70a99541ef27e
Thiago_Moreira <164171664+ThiagoDevMoreira@users.noreply.github.com>
2025-03-26 06:47:23 -0300

    Update exercicio1-somaDeNumerosCosnecutivos.py

--- exercicio1-somaDeNumerosCosnecutivos.py
+++ exercicio1-somaDeNumerosCosnecutivos.py
@@ -1,15 +1,26 @@
-# Script em pyton correspondente ao exercício 1
-def calcular_soma(numero):
+# 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+# Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+# Imprimir(SOMA);
+# Ao final do processamento, qual será o valor da variável SOMA?
+# ** Resposta: no final a soma é 91**
+
+# script em pyton correspondente ao exercício 1
+def calcular_soma(num):
     soma = 0
     k = 0
     
-    while k < numero:
+    while k < num:
         k += 1
         soma += k
     
     return soma
     
 numero = int(input("Digite um número inteiro: "))
-resultado = calcular_soma(numero)
+resultado = calcular_soma(num)
 
-print(f"O valor da soma dos números de 1 a {numero} é: {resultado}")
+print(f"O valor da soma dos números de 1 a {num} é: {resultado}")
+
+# outra forma de fazer isso
+def calcular_soma_sem_loop(num)
+    return (num + 1) * (num / 2)
+    
